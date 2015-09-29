3061a3c7ab77120722783cad6b320bcc31c0a725
kohelet-net-admin <git-master@kohelet-network.org>
2015-09-30 00:37:05 +0200

    Added tools project for Zefania Xml data

---
+++ 0110_RegisterZefaniaXmlFiles/Main.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim baseDir As String = System.IO.Path.Combine(System.Environment.CurrentDirectory, "..\..\..\..\Zefania-Xml-Bibles\Bibles")
+        Dim XmlIndexFile As String = System.IO.Path.Combine(baseDir, "index.txt")
+        Dim XsdSchemaFile As String = System.IO.Path.Combine(baseDir, "..\Schema\zef2014.xsd")
+        Console.SetWindowSize(Console.WindowWidth * 2.5, Console.WindowHeight * 2.5)
+        Console.InputEncoding = System.Text.Encoding.UTF8
+        Console.OutputEncoding = System.Text.Encoding.UTF8
+        Try
+            If System.IO.File.Exists(XmlIndexFile) = False Then
+                Console.WriteLine("MISSING XML INDEX FILE: " & New IO.FileInfo(XmlIndexFile).FullName)
+                System.Environment.Exit(1)
+            End If
+            Dim xmlFiles As String() = System.IO.File.ReadAllLines(XmlIndexFile)
+            For Each XmlFile As String In xmlFiles
+                Dim FullXmlFilePath As String = System.IO.Path.Combine(baseDir, XmlFile)
+                If System.IO.File.Exists(FullXmlFilePath) = False Then
+                    Dim ForegroundDefaulColor As ConsoleColor = Console.ForegroundColor
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("MISSING XML FILE: " & FullXmlFilePath)
+                    Console.ForegroundColor = ConsoleColor.Black
+                    System.Environment.Exit(2)
+                Else
+                    Console.WriteLine("USING XML FILE: " & XmlFile)
+                    Console.WriteLine("Validation Result: " & System.Enum.GetName(GetType(XPathValidation.ValidationResult), XPathValidation.ValidateXml(XsdSchemaFile, FullXmlFilePath)))
+                    Console.WriteLine()
+                End If
+            Next
+        Catch ex As Exception
+            Dim ForegroundDefaulColor As ConsoleColor = Console.ForegroundColor
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("Warning {0}", ex.Message)
+            Console.ForegroundColor = ConsoleColor.Black
+            System.Environment.Exit(3)
+        End Try
+    End Sub
+
+End Module
---
+++ 0110_RegisterZefaniaXmlFiles/XPathValidation.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports System.Xml
+Imports System.Xml.Schema
+Imports System.Xml.XPath
+
+Class XPathValidation
+
+    Public Enum ValidationResult As Byte
+        NoWarnings = 0
+        HasWarnings = 1
+        HasErrors = 2
+    End Enum
+
+    Shared Function ValidateXml(schemaFile As String, xmlFile As String) As ValidationResult
+        Try
+            Dim settings As XmlReaderSettings = New XmlReaderSettings()
+            settings.Schemas.Add("http://www.bgfdb.de/zefaniaxml/2014/", schemaFile)
+            settings.ValidationType = ValidationType.Schema
+
+            Dim reader As XmlReader = XmlReader.Create(xmlFile, settings)
+            Dim document As XmlDocument = New XmlDocument()
+            document.Load(reader)
+
+            Dim eventHandler As ValidationEventHandler = New ValidationEventHandler(AddressOf ValidationEventHandler)
+            document.Validate(eventHandler)
+        Catch ex As Exception
+            ValidationErrors.Add(ex)
+            Return ValidationResult.HasErrors
+        End Try
+        If ValidationErrors.Count > 0 Then
+            Return ValidationResult.HasErrors
+        ElseIf ValidationWarnings.Count > 0 Then
+            Return ValidationResult.HasWarnings
+        Else
+            Return ValidationResult.NoWarnings
+        End If
+    End Function
+
+    Public Shared ReadOnly Property ValidationWarnings As New List(Of Exception)
+    Public Shared ReadOnly Property ValidationErrors As New List(Of Exception)
+
+    Private Shared Sub ValidationEventHandler(ByVal sender As Object, ByVal e As ValidationEventArgs)
+        Select Case e.Severity
+            Case XmlSeverityType.Error
+                ValidationErrors.Add(e.Exception)
+            Case XmlSeverityType.Warning
+                ValidationWarnings.Add(e.Exception)
+        End Select
+
+    End Sub
+
+End Class
