98b97d2d6b42e90c60dc8306ddef91df47a893b4
Kohelet Network Admin <git-master@kohelet-network.org>
2019-07-21 14:31:51 +0200

    Update of dependencies

---
+++ 0110_RegisterZefaniaXmlFiles/AssemblyInfo.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("ZefaniaXmlTools")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ZefaniaXmlTools")>
+<Assembly: AssemblyCopyright("Copyright ©  2015,2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+<Assembly: Guid("28ed3036-97ca-42fd-b6b2-5d0b0ae9157b")>
+
+<Assembly: AssemblyVersion("2019.07.21.100")>
+
