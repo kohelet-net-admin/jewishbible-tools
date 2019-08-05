0f61c7c2851f02793772d7f2b0c5d46aff6d0087
Kohelet Network Admin <git-master@kohelet-network.org>
2019-08-05 23:24:41 +0200

    Initial commit for CSV config file editor tool

---
+++ 0200_Level100Bibles/CsvSheetEditor/CsvSheetEditor/CsvEditor.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CsvEditor
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.CsvDataGridView = New System.Windows.Forms.DataGridView()
+        Me.CsvOpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.CsvSaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip.SuspendLayout()
+        CType(Me.CsvDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip.TabIndex = 0
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.SaveasToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenToolStripMenuItem.Text = "&Open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Enabled = False
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SaveToolStripMenuItem.Text = "&Save"
+        '
+        'SaveasToolStripMenuItem
+        '
+        Me.SaveasToolStripMenuItem.Enabled = False
+        Me.SaveasToolStripMenuItem.Name = "SaveasToolStripMenuItem"
+        Me.SaveasToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SaveasToolStripMenuItem.Text = "Save &as..."
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(177, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip.TabIndex = 1
+        '
+        'CsvDataGridView
+        '
+        Me.CsvDataGridView.AllowUserToOrderColumns = True
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlLight
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.CsvDataGridView.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.CsvDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.CsvDataGridView.DefaultCellStyle = DataGridViewCellStyle2
+        Me.CsvDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CsvDataGridView.Location = New System.Drawing.Point(0, 24)
+        Me.CsvDataGridView.Name = "CsvDataGridView"
+        Me.CsvDataGridView.Size = New System.Drawing.Size(800, 404)
+        Me.CsvDataGridView.TabIndex = 2
+        '
+        'CsvOpenFileDialog
+        '
+        Me.CsvOpenFileDialog.Filter = "JewishBibleTool CSV configuration files (*.csv)|*.csv"
+        Me.CsvOpenFileDialog.Title = "Open CSV configuration file"
+        '
+        'CsvSaveFileDialog
+        '
+        Me.CsvSaveFileDialog.Filter = "JewishBibleTool CSV configuration files (*.csv)|*.csv"
+        Me.CsvSaveFileDialog.Title = "Save CSV configuration file"
+        '
+        'CsvEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CsvDataGridView)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "CsvEditor"
+        Me.Text = "CSV Sheet-Editor"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        CType(Me.CsvDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents CsvDataGridView As DataGridView
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CsvOpenFileDialog As OpenFileDialog
+    Friend WithEvents CsvSaveFileDialog As SaveFileDialog
+End Class
---
+++ 0200_Level100Bibles/CsvSheetEditor/CsvSheetEditor/CsvEditor.vb
@@ -0,0 +1,93 @@
+﻿Public Class CsvEditor
+
+    Public PathOfLoadedData As String = Nothing
+
+    Public Sub New()
+
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If Me.CsvOpenFileDialog.InitialDirectory = Nothing Then Me.CsvOpenFileDialog.InitialDirectory = System.Environment.CurrentDirectory
+        Dim Result As DialogResult = Me.CsvOpenFileDialog.ShowDialog
+        If Result = DialogResult.OK Then
+            Dim CsvPath As String = Nothing
+            CsvPath = Me.CsvOpenFileDialog.FileName
+            Try
+                Me.CsvDataGridView.DataSource = CompuMaster.Data.Csv.ReadDataTableFromCsvFile(CsvPath, True, CompuMaster.Data.Csv.ReadLineEncodings.RowBreakCrLfOrLf_CellLineBreakCr, CompuMaster.Data.Csv.ReadLineEncodingAutoConversion.NoAutoConversion, "UTF-8", ","c, """"c, False, False)
+                PathOfLoadedData = CsvPath
+                Me.SaveToolStripMenuItem.Enabled = True
+                Me.SaveasToolStripMenuItem.Enabled = True
+                Me.CsvDataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells)
+                Me.CsvDataGridView.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells)
+            Catch ex As Exception
+                Me.CsvDataGridView.DataSource = Nothing
+                PathOfLoadedData = Nothing
+                MsgBox(ex.Message, MsgBoxStyle.Critical Or MsgBoxStyle.OkOnly)
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        If PathOfLoadedData = Nothing Then
+            SaveasToolStripMenuItem_Click(sender, e)
+        Else
+            Try
+                'Save current sheet
+                Dim dt As System.Data.DataTable = CType(Me.CsvDataGridView.DataSource, System.Data.DataTable)
+                CompuMaster.Data.Csv.WriteDataTableToCsvFile(PathOfLoadedData, dt, True, CompuMaster.Data.Csv.WriteLineEncodings.RowBreakCrLf_CellLineBreakSpaceChar, "UTF-8", ","c, """"c, "."c)
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Critical Or MsgBoxStyle.OkOnly)
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveasToolStripMenuItem.Click
+        SaveCurrentCsvFileAs()
+    End Sub
+
+    ''' <summary>
+    ''' Save the file at a new location
+    ''' </summary>
+    ''' <returns>True if saved successfully, False if user aborted or error</returns>
+    Private Function SaveCurrentCsvFileAs() As Boolean
+        Me.CsvOpenFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(Me.PathOfLoadedData)
+        Dim Result As DialogResult = Me.CsvSaveFileDialog.ShowDialog
+        If Result = DialogResult.OK Then
+            Dim NewCsvPath As String
+            NewCsvPath = Me.CsvSaveFileDialog.FileName
+            Try
+                'Save current sheet at another path
+                Dim dt As System.Data.DataTable = CType(Me.CsvDataGridView.DataSource, System.Data.DataTable)
+                CompuMaster.Data.Csv.WriteDataTableToCsvFile(NewCsvPath, dt, True, CompuMaster.Data.Csv.WriteLineEncodings.RowBreakCrLf_CellLineBreakSpaceChar, "UTF-8", ","c, """"c, "."c)
+                PathOfLoadedData = NewCsvPath
+                Return True
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Critical Or MsgBoxStyle.OkOnly)
+                Return False
+            End Try
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        If Me.CsvDataGridView.DataSource IsNot Nothing AndAlso CType(Me.CsvDataGridView.DataSource, System.Data.DataTable).GetChanges.Rows.Count > 0 Then
+            'Changes found - save file?
+            Select Case MsgBox("Quit with saving changes?", MsgBoxStyle.YesNoCancel Or MsgBoxStyle.Question)
+                Case MsgBoxResult.Yes
+                    If SaveCurrentCsvFileAs() Then System.Environment.Exit(0)
+                Case MsgBoxResult.No
+                    System.Environment.Exit(0)
+            End Select
+        Else
+            'No changes found - simply quit
+            System.Environment.Exit(0)
+        End If
+    End Sub
+
+End Class
