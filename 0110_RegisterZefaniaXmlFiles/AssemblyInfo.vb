60f851ceb4ae663c4115b9fe3a9bb967ed5bd63f
Kohelet Network Admin <git-master@kohelet-network.org>
2019-08-11 10:28:48 +0200

    Update AssemblyInfo.vb

--- 0110_RegisterZefaniaXmlFiles/AssemblyInfo.vb
+++ 0110_RegisterZefaniaXmlFiles/AssemblyInfo.vb
@@ -4,13 +4,13 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("ZefaniaXmlTools")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyCompany("Kohelet-Network")>
 <Assembly: AssemblyProduct("ZefaniaXmlTools")>
-<Assembly: AssemblyCopyright("Copyright ©  2015,2019")>
+<Assembly: AssemblyCopyright("Copyright © 2015,2019")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 <Assembly: Guid("28ed3036-97ca-42fd-b6b2-5d0b0ae9157b")>
 
-<Assembly: AssemblyVersion("2019.07.21.100")>
+<Assembly: AssemblyVersion("2019.08.11.100")>
 
